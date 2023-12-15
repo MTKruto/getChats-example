b9c66efa48e6be3a50d94a05f0bf95c22c72477b
Roj <ez@roj.im>
2023-12-15 19:25:29 +0300

    Load more chats when the end of the list is reached

--- components/ChatList/ChatList.tsx
+++ components/ChatList/ChatList.tsx
@@ -1,15 +1,34 @@
 import { Chat } from "./Chat.tsx";
-import { chats } from "../../state/chats.ts";
+import { chats, loadChats } from "../../state/chats.ts";
 import { useAutoAnimate } from "@formkit/auto-animate/preact";
+import { useEffect } from "preact/hooks";
+import { createRef } from "preact";
 
 export function ChatList() {
+  const divRef = createRef<HTMLDivElement>();
   const [parent] = useAutoAnimate();
 
+  useEffect(() => {
+    const div = divRef.current;
+    if (!div) {
+      return;
+    }
+    const onScroll = () => {
+      if (div.getBoundingClientRect().bottom <= window.innerHeight) {
+        loadChats();
+      }
+    };
+    document.addEventListener("scroll", onScroll);
+    return () => document.removeEventListener("scroll", onScroll);
+  }, []);
+
   return (
-    <div ref={parent} class="flex flex-col sm:border-x border-[#2C3848]">
-      {[...chats.value.values()]
-        .sort((a, b) => b.order.localeCompare(a.order))
-        .map((chat) => <Chat key={chat.id}>{chat}</Chat>)}
+    <div ref={divRef}>
+      <div ref={parent} class="flex flex-col sm:border-x border-[#2C3848]">
+        {[...chats.value.values()]
+          .sort((a, b) => b.order.localeCompare(a.order))
+          .map((chat) => <Chat key={chat.id}>{chat}</Chat>)}
+      </div>
     </div>
   );
 }
--- state/chats.ts
+++ state/chats.ts
@@ -1,6 +1,7 @@
 import { Chat } from "mtkruto/mod.ts";
 import { signal } from "@preact/signals";
 import { client } from "../client.ts";
+import { Mutex } from "async-mutex";
 
 export const chats = signal(new Map<number, Chat>());
 
@@ -12,8 +13,26 @@ client.on("editedChat", (ctx) => {
   chats.value = new Map(chats.value.set(ctx.editedChat.id, ctx.editedChat));
 });
 
+let after: Chat | undefined = undefined;
+let allLoaded = false;
+const mutex = new Mutex();
 export async function loadChats() {
-  for (const chat of await client.getChats()) {
-    chats.value = new Map(chats.value.set(chat.id, chat));
+  if (allLoaded) {
+    return;
+  }
+  const release = await mutex.acquire();
+  try {
+    const limit = 100;
+    const chats_ = await client.getChats({ after, limit });
+    for (const chat of chats_) {
+      chats.value = new Map(chats.value.set(chat.id, chat));
+    }
+    if (chats_.length < limit) {
+      allLoaded = true;
+    } else {
+      after = chats_[chats_.length - 1];
+    }
+  } finally {
+    release();
   }
 }
